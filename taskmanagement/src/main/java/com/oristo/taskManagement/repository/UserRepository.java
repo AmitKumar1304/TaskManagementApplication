4397f08121a3c01c82ca2aa67bded515c62f7f5b
Amit Kumar <99786819+AmitKumar1304@users.noreply.github.com>
2025-06-06 23:15:03 +0530

    Delete taskmanagement directory

--- taskmanagement/src/main/java/com/oristo/taskManagement/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oristo.taskManagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.oristo.TaskManagement.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
